1ee9819e8d4551e3cbbcd0de5717f4c0b1995b9d
H.Z <15738818183@163.com>
2019-07-01 18:06:35 +0800

    Breadcrumb

--- src/components/Layout/Breadcrumb/index.jsx
+++ src/components/Layout/Breadcrumb/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import { Breadcrumb } from 'antd'
+import { Breadcrumb, Icon } from 'antd'
+import { Link } from 'react-router-dom'
 import './index.less'
 
 const AdminBreadcrumb = () => {
   return (
     <Breadcrumb className="admin-breadcrumb">
-      <Breadcrumb.Item>User</Breadcrumb.Item>
+      <Breadcrumb.Item>
+        <Link to={'/'}>
+          <Icon type="home" /> User
+        </Link>
+      </Breadcrumb.Item>
       <Breadcrumb.Item>Bill</Breadcrumb.Item>
     </Breadcrumb>
   )
--- src/containers/Tables/Table1/index.jsx
+++ src/containers/Tables/Table1/index.jsx
@@ -1,5 +1,20 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
+// 把redux 里面的数据映射到 props
+const mapStateToProps = (state) => {
+  return {
+    tes1111111111: '哈哈哈'
+  }
+}
+
+// dispatch 映射到props
+const mapDispatchToProps = dispatch => {
+  return {
+  }
+}
+
+@connect(mapStateToProps, mapDispatchToProps)
 class Table1 extends Component {
   render() {
     return (
